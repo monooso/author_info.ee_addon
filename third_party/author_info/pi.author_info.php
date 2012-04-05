5400778cad52f4b3b2f1bc4cca4333f7def771e2
Stephen Lewis <stephen@experienceinternet.co.uk>
2012-04-05 16:41:39 +0100

    Implement plugin tag.

--- third_party/author_info/pi.author_info.php
+++ third_party/author_info/pi.author_info.php
@@ -11,7 +11,8 @@
 $plugin_info = array(
   'pi_author'       => 'Stephen Lewis',
   'pi_author_url'   => 'http://experienceinternet.co.uk/',
-  'pi_description'  => 'Provides additional information about the author of a Channel entry.',
+  'pi_description'  => 'Provides additional information about the author of a'
+                        .' Channel entry.',
   'pi_name'         => 'Author Info',
   'pi_usage'        => Author_info::usage(),
   'pi_version'      => '0.1.0'
@@ -62,6 +63,24 @@ class Author_info {
 
     $this->EE->load->model('author_info_plugin_model');
     $this->_pi_model = $this->EE->author_info_plugin_model;
+
+    // Retrieve the entry ID.
+    if ( ! $entry_id = $this->EE->TMPL->fetch_param('entry_id'))
+    {
+      $this->return_data = $this->EE->TMPL->no_results();
+      return;
+    }
+
+    // Retrieve the member info.
+    if ( ! $author = $this->_pi_model->get_author_info_from_entry_id($entry_id))
+    {
+      $this->return_data = $this->EE->TMPL->no_results();
+      return;
+    }
+
+    // Parse the tagdata.
+    $this->return_data = $this->EE->TMPL->parse_variables_row(
+      $this->EE->TMPL->tagdata, $author->to_array('author:'));
   }
 
 
--- third_party/author_info/tests/test.pi_author_info.php
+++ third_party/author_info/tests/test.pi_author_info.php
@@ -14,8 +14,6 @@ require_once PATH_THIRD .'author_info/models/author_info_plugin_model.php';
 class Test_author_info extends Testee_unit_test_case {
 
   private $_pi_model;
-  private $_subject;
-
 
   /* --------------------------------------------------------------
    * PUBLIC METHODS
@@ -42,9 +40,90 @@ class Test_author_info extends Testee_unit_test_case {
      */
 
     $this->EE->author_info_plugin_model = $this->_get_mock('plugin_model');
-
     $this->_pi_model  = $this->EE->author_info_plugin_model;
-    $this->_subject   = new Author_info();
+  }
+
+
+  public function test__default_tag__retrieves_entry_id_parameter()
+  {
+    $entry_id = FALSE;    // Ensures we go no further.
+    $tmpl = $this->EE->TMPL;
+
+    $tmpl->expectOnce('fetch_param', array('entry_id'));
+    $tmpl->returns('fetch_param', $entry_id, array('entry_id'));
+
+    $subject = new Author_info();
+  }
+
+
+  public function test__default_tag__returns_no_results_if_no_entry_id_parameter()
+  {
+    $no_results = 'No results.';
+    $tmpl       = $this->EE->TMPL;
+
+    $this->_pi_model->expectNever('get_author_info_from_entry_id');
+
+    $tmpl->returns('fetch_param', FALSE, array('entry_id'));
+    $tmpl->returns('no_results', $no_results);
+
+    $subject = new Author_info();
+    $this->assertIdentical($no_results, $subject->return_data);
+  }
+
+
+  public function test__default_tag__returns_no_results_if_model_returns_false()
+  {
+    $entry_id   = '10';
+    $no_results = 'No results.';
+    $tmpl       = $this->EE->TMPL;
+
+    $tmpl->returns('fetch_param', $entry_id, array('entry_id'));
+    $tmpl->returns('no_results', $no_results);
+
+    $this->_pi_model->expectOnce('get_author_info_from_entry_id',
+      array($entry_id));
+
+    $this->_pi_model->returns('get_author_info_from_entry_id', FALSE);
+
+    $subject = new Author_info();
+    $this->assertIdentical($no_results, $subject->return_data);
+  }
+
+
+  public function test__default_tag__retrieves_member_data_and_parses_tagadata()
+  {
+    $author = new Author(array(
+      'email'       => 'bill@microsoft.com',
+      'group_id'    => 10,
+      'is_admin'    => FALSE,
+      'member_id'   => 123,
+      'screen_name' => 'Bill Gates',
+      'username'    => 'bill.i.am'
+    ));
+
+    $entry_id   = '10';
+    $parsed     = '<p>Parsed tagdata</p>';
+    $tagdata    = '<p>Example tagdata.</p>';
+    $tmpl       = $this->EE->TMPL;
+
+    // Retrieve the entry ID parameter.
+    $tmpl->returns('fetch_param', $entry_id, array('entry_id'));
+
+    // Retrieve the member info.
+    $this->_pi_model->expectOnce('get_author_info_from_entry_id',
+      array($entry_id));
+
+    $this->_pi_model->returns('get_author_info_from_entry_id', $author);
+
+    // Retrieve and parse the tagdata.
+    $tmpl->tagdata = $tagdata;    // Still no getter.
+    $template_data = $author->to_array('author:');
+
+    $tmpl->expectOnce('parse_variables_row', array($tagdata, $template_data));
+    $tmpl->returns('parse_variables_row', $parsed);
+
+    $subject = new Author_info();
+    $this->assertIdentical($parsed, $subject->return_data);
   }
 
 
